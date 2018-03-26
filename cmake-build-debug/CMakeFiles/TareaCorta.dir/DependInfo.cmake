b7ecf010796845e49f00d7d6949fd8633656092c
aalopz02 <aalopz02@gmail.com>
2018-03-26 14:50:01 -0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/TareaCorta.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/TareaCorta_autogen/mocs_compilation.cpp" "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/CMakeFiles/TareaCorta.dir/TareaCorta_autogen/mocs_compilation.cpp.o"
+  "/home/andres/CLionProjects/TareaCorta/Window.cpp" "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/CMakeFiles/TareaCorta.dir/Window.cpp.o"
+  "/home/andres/CLionProjects/TareaCorta/logic/guardarPagina.cpp" "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/CMakeFiles/TareaCorta.dir/logic/guardarPagina.cpp.o"
+  "/home/andres/CLionProjects/TareaCorta/logic/lectorTextEdit.cpp" "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/CMakeFiles/TareaCorta.dir/logic/lectorTextEdit.cpp.o"
+  "/home/andres/CLionProjects/TareaCorta/logic/leerPagina.cpp" "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/CMakeFiles/TareaCorta.dir/logic/leerPagina.cpp.o"
+  "/home/andres/CLionProjects/TareaCorta/logic/mServer.cpp" "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/CMakeFiles/TareaCorta.dir/logic/mServer.cpp.o"
+  "/home/andres/CLionProjects/TareaCorta/logic/memReserver.cpp" "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/CMakeFiles/TareaCorta.dir/logic/memReserver.cpp.o"
+  "/home/andres/CLionProjects/TareaCorta/logic/pagina.cpp" "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/CMakeFiles/TareaCorta.dir/logic/pagina.cpp.o"
+  "/home/andres/CLionProjects/TareaCorta/main.cpp" "/home/andres/CLionProjects/TareaCorta/cmake-build-debug/CMakeFiles/TareaCorta.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "TareaCorta_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TareaCorta.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "TareaCorta_autogen"
+  "CMakeFiles/TareaCorta_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/TareaCorta.dir/main.cpp.o"
+  "CMakeFiles/TareaCorta.dir/logic/mServer.cpp.o"
+  "CMakeFiles/TareaCorta.dir/logic/memReserver.cpp.o"
+  "CMakeFiles/TareaCorta.dir/logic/guardarPagina.cpp.o"
+  "CMakeFiles/TareaCorta.dir/Window.cpp.o"
+  "CMakeFiles/TareaCorta.dir/logic/pagina.cpp.o"
+  "CMakeFiles/TareaCorta.dir/logic/leerPagina.cpp.o"
+  "CMakeFiles/TareaCorta.dir/logic/lectorTextEdit.cpp.o"
+  "CMakeFiles/TareaCorta.dir/TareaCorta_autogen/mocs_compilation.cpp.o"
+  "TareaCorta.pdb"
+  "TareaCorta"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TareaCorta.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
