744fadbacf998153419c506023b996c67ece489f
aalopz02 <aalopz02@gmail.com>
2018-04-02 00:25:56 -0600

    Delete all_0.js

--- TareaCorta/doc/html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['agregarpagina',['agregarPagina',['../classm_server.html#ab2077d95ca266157d572c327fb93e7e6',1,'mServer']]],
-  ['agregarpaginacompleta',['agregarPaginaCompleta',['../classm_server.html#ac3ecd22229fcc54f3f6f22aab57db1a4',1,'mServer']]]
-];
