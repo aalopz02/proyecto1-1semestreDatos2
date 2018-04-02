da9de38c06c3aec4d18bd618915f9720a02312f6
aalopz02 <aalopz02@gmail.com>
2018-04-02 00:26:08 -0600

    Delete all_1.js

--- TareaCorta/doc/html/search/all_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['buscararchivo',['buscarArchivo',['../classleer_pagina.html#ac1d0350d60b20a70500a6ffb1078783a',1,'leerPagina']]],
-  ['buscarlinea',['buscarLinea',['../classleer_pagina.html#a9d91208a6e7eb986394b6491384efa92',1,'leerPagina']]],
-  ['buscarlineaaux',['buscarLineaAux',['../classleer_pagina.html#abbeafc685dfd2dcfc36c656ad5c27690',1,'leerPagina']]]
-];
