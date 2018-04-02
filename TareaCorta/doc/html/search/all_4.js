d0f636362b32267820926a22fed9f1ce172d08c7
aalopz02 <aalopz02@gmail.com>
2018-04-02 00:26:14 -0600

    Delete all_4.js

--- TareaCorta/doc/html/search/all_4.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['generarpagina',['generarPagina',['../classguardar_pagina.html#a344328cea47595c25a7ecb18dfed2a57',1,'guardarPagina']]],
-  ['getmemserver',['getMemServer',['../classm_server.html#a175c535017a8b559eeceb47d27d6da28',1,'mServer']]],
-  ['guardarpagina',['guardarPagina',['../classguardar_pagina.html',1,'guardarPagina'],['../classguardar_pagina.html#a3b970b12d4a000ae41f7049fc53bfa0b',1,'guardarPagina::guardarPagina()']]],
-  ['guardarpaginadisco',['guardarPaginaDisco',['../classguardar_pagina.html#a02c9e3e2eebca935f1aafe8afc334af3',1,'guardarPagina']]]
-];
