20b5055937151b94e8e769dcda732c42355e546d
aalopz02 <aalopz02@gmail.com>
2018-04-07 23:34:23 -0600

    Delete all_8.js

--- TareaCorta/doc/html/search/all_8.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['obtenepagina',['obtenepagina',['../classleer_pagina.html#af93635115105afe524e89bfb75d8174d',1,'leerPagina']]],
-  ['obtenercantidadpag',['obtenerCantidadPag',['../classm_server.html#a67997f7af71ce2d61cc4cb06592874d8',1,'mServer']]],
-  ['obtenercontenido',['obtenerContenido',['../classpagina.html#a1f53b71a3ba714249064b0de25c81149',1,'pagina']]],
-  ['obtenermemoria',['obtenerMemoria',['../classmem_reserver.html#af22c2308ae39dedde54b41f900318114',1,'memReserver']]],
-  ['obtenernumero',['obtenerNumero',['../classpagina.html#a20241a4d6dfa1ebddfbdb15395bd5e75',1,'pagina']]],
-  ['obtenerpaginasuso',['obtenerPaginasUso',['../classm_server.html#a027377abae120452312130fe673efa67',1,'mServer']]]
-];
