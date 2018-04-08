509ad0bf1da7ae9bd055bd8ac88b341762596f9b
aalopz02 <aalopz02@gmail.com>
2018-04-07 23:34:04 -0600

    Delete all_6.js

--- TareaCorta/doc/html/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['leerpagina',['leerPagina',['../classleer_pagina.html',1,'leerPagina'],['../classleer_pagina.html#ab1009387aa6c3d1ad7429768faafa811',1,'leerPagina::leerPagina()']]]
-];
